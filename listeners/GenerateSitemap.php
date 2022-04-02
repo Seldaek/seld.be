1d4cb67ee762601823827883a3722dda958c8c76
Jordi Boggiano <j.boggiano@seld.be>
2022-04-02 13:52:15 +0200

    Disable jekyll

--- listeners/GenerateSitemap.php
+++ listeners/GenerateSitemap.php
@@ -14,6 +14,7 @@ class GenerateSitemap
         '/pavatar.png',
         '/google43a461bfa3176540.html',
         '/.well-known/*',
+        '/.nojekyll',
         '/CNAME',
         '*/404*'
     ];
